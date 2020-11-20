777337cc984ef12a2d268a037795669bb81f3599
AmineBenaddiGitHub <amine.benaddi@gmail.com>
2020-11-20 09:40:59 +0100

    react-native-phone-input removed

--- src/components/PhoneInput/index.tsx
+++ src/components/PhoneInput/index.tsx
@@ -1,13 +1,14 @@
 import React, { useRef } from 'react';
 import { TextInput } from 'react-native';
-import PhoneInput from 'react-native-phone-input';
-import countriesList from './countries.json';
+// import PhoneInput from 'react-native-phone-input';
+// import countriesList from './countries.json';
 
 const PhoneInputText = () => {
-  const phoneInputRef = useRef<PhoneInput<typeof TextInput> | null | undefined>(
+  const phoneInputRef = useRef<TextInput | null | undefined>(
     null,
   );
-  return <PhoneInput initialCountry="fr" countriesList={countriesList} />;
+  // return <PhoneInput initialCountry="fr" countriesList={countriesList} />;
+  return <TextInput />;
 };
 
 export default PhoneInputText;
