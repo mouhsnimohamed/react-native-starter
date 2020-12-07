0f622443497eff3f8456c298052768c54841d4b4
mohamed mouhsni <m.mouhsni@gmail.com>
2020-12-07 21:05:58 +0100

    clean project

--- src/screens/NotAuthenticated/stack-navigator.tsx
+++ src/screens/NotAuthenticated/stack-navigator.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Signin from './Signin';
 import Signup from './Signup';
-import PreActivateAccountScreen from './Signin/PreActivateAccount';
-import ActivateAccountScreen from './ActivateAccount';
 import WarningScreen from './WarningScreen';
 
 import { RootStackParamList } from 'navigations/RootNavigation';
@@ -32,11 +30,6 @@ const NotAuthenticatedStackNavigator: React.FC<NotAuthenticatedStackNavigatorPro
       />
       <Stack.Screen name="Signup" component={Signup} />
       <Stack.Screen name="Warning" component={WarningScreen} />
-      <Stack.Screen
-        name="PreActivateAccount"
-        component={PreActivateAccountScreen}
-      />
-      <Stack.Screen name="ActivateAccount" component={ActivateAccountScreen} />
     </Stack.Navigator>
   );
 };
